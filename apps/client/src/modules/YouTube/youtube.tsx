c9fc0e3286d8bc671ee9ca4c4830efb418c0bd3b
Idhibhat Pankam <marginpankam@gmail.com>
2023-09-06 23:07:24 +0700

    some frame

--- apps/client/src/modules/YouTube/youtube.tsx
+++ apps/client/src/modules/YouTube/youtube.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { Flex } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Box, Flex } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 import ReactPlayer from "react-player";
 import YouTubePlayer from "react-player/youtube";
 
 export function Youtube(): JSX.Element {
   const [hasWindow, setHasWindow] = useState(false);
   const [playing, setPlaying] = useState(false);
+  const imageRef = useRef<HTMLImageElement>(null);
   useEffect(() => {
     if (typeof window !== "undefined") {
       setHasWindow(true);
@@ -18,11 +20,46 @@ export function Youtube(): JSX.Element {
     setPlaying(!playing);
   };
   return (
-    <Flex onClick={handleClick}>
-      {/* <YouTubePlayer url="https://www.youtube.com/watch?v=zO6rqiPSV2g" /> */}
-      {hasWindow ? (
+    <Flex
+      backgroundColor="red"
+      height="100vh"
+      onClick={handleClick}
+      onKeyDown={handleClick}
+      width="100vw"
+    >
+      {/* {hasWindow ? (
         <ReactPlayer playing={playing} url="./mite_kure.mp4" volume={1} />
-      ) : null}
+      ) : null} */}
+      <Image
+        alt="frame"
+        height={1000}
+        ref={imageRef}
+        src="/green_screen.png"
+        style={{ zIndex: 100, objectFit: "cover", width: "100vw" }}
+        width={1000}
+      />
+      <Box position="absolute" top="10vh" left="20vh">
+        {hasWindow ? (
+          <ReactPlayer
+            config={{
+              youtube: {
+                playerVars: {
+                  showinfo: 0,
+                  controls: 0,
+                  rel: 0,
+                  modestbranding: 1,
+                  iv_load_policy: 3,
+                  fs: 0,
+                  loop: 1,
+                },
+              },
+            }}
+            controls={false}
+            playing={playing}
+            url="https://www.youtube.com/watch?v=zO6rqiPSV2g"
+          />
+        ) : null}
+      </Box>
     </Flex>
   );
 }
