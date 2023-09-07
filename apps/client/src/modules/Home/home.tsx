c4199f85f3bd44459b770794c4d00d1ae527202b
Idhibhat Pankam <marginpankam@gmail.com>
2023-09-07 13:28:46 +0700

    bg video

--- apps/client/src/modules/Home/home.tsx
+++ apps/client/src/modules/Home/home.tsx
@@ -9,13 +9,22 @@ import {
 } from "@chakra-ui/react";
 import { CopyIcon } from "@chakra-ui/icons";
 import Link from "next/link";
-import { useState } from "react";
+import ReactPlayer from "react-player";
+import { useState, useEffect } from "react";
 import { h1 } from "../../theme/font-sizes";
 
 export function Home(): JSX.Element {
   const [url, setUrl] = useState("");
   const [showView, setShowView] = useState(false);
   const [error, setError] = useState(false);
+  const [hasWindow, setHasWindow] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
+
   const generate = (): void => {
     try {
       const _ = new URL(url);
@@ -39,16 +48,35 @@ export function Home(): JSX.Element {
       justifyContent="center"
       w="100vw"
     >
-      <Text fontSize={h1} fontWeight="bold" mb="7vh">
+      {hasWindow ? (
+        <ReactPlayer
+          controls={false}
+          height="100%"
+          loop
+          muted
+          playing
+          style={{
+            position: "absolute",
+            zIndex: -1,
+            filter: "blur(8px) opacity(0.5)",
+          }}
+          url="./mite_kure.mp4"
+          width="100%"
+        />
+      ) : null}
+      <Text color="black" fontSize={h1} fontWeight="bold" mb="7vh">
         MITE KURE Generator
       </Text>
       <Flex gap="2vw" justify="center" mb="4vh" w="100%">
         <FormControl isInvalid={error} width="50%">
           <Input
+            _placeholder={{ color: "gray.600" }}
+            borderColor="gray.600"
             onChange={(e) => {
               setUrl(e.target.value);
             }}
             placeholder="YouTube URL"
+            textColor="black"
             value={url}
           />
           {error ? <FormErrorMessage>URL is not valid</FormErrorMessage> : null}
@@ -61,8 +89,10 @@ export function Home(): JSX.Element {
       {showView ? (
         <Flex gap="1vw" justify="center" w="100%">
           <Input
+            borderColor="gray.600"
             placeholder="YouTube URL"
             readOnly
+            textColor="black"
             value={`${window.location.origin}watch?url=${url}`}
             w="50%"
           />
