48af67ed5e75f08c4ea9f72139c45e89f80c2eb0
Idhibhat Pankam <marginpankam@gmail.com>
2023-09-07 13:10:38 +0700

    favicon

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Mite-Kure",
-  description: "",
+  description: "ああぁぁぁぁぁぁぁぁ　見てくれ！",
 };
 
 interface RootLayoutProps {
@@ -16,6 +16,26 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
   return (
     <html lang="en">
+      <link
+        href="favicon/apple-touch-icon.png"
+        rel="apple-touch-icon"
+        sizes="180x180"
+      />
+      <link
+        href="favicon/favicon-32x32.png"
+        rel="icon"
+        sizes="32x32"
+        type="image/png"
+      />
+      <link
+        href="favicon/favicon-16x16.png"
+        rel="icon"
+        sizes="16x16"
+        type="image/png"
+      />
+      <link href="favicon/site.webmanifest" rel="manifest" />
+      <meta content="#da532c" name="msapplication-TileColor" />
+      <meta content="#ffffff" name="theme-color" />
       <body>
         <Analytics />
         <Providers>
