92004af343bd9bae194e869c83c8f7dc62ae0c17
Idhibhat Pankam <marginpankam@gmail.com>
2023-09-07 21:29:56 +0700

    fix: video plays twice

--- apps/client/src/modules/Watch/watch.tsx
+++ apps/client/src/modules/Watch/watch.tsx
@@ -1,16 +1,11 @@
 "use client";
 import { Flex } from "@chakra-ui/react";
-import { useEffect } from "react";
 import { useAppContext } from "../../core/context/app-context";
 import { Intro } from "../Intro";
 import { Youtube } from "../YouTube";
 
 export function Watch(): JSX.Element {
-  const { setIntroDone, introDone, setStartYouTube } = useAppContext();
-  useEffect(() => {
-    setIntroDone(false);
-    setStartYouTube(false);
-  }, [setIntroDone, setStartYouTube]);
+  const { introDone } = useAppContext();
 
   return (
     <Flex alignItems="center" h="100vh" justifyContent="center" w="100vw">
